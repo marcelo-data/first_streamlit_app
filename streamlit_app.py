367d19faab84b01118c66574629c5bb32df6c414
marcelo-data <126196376+marcelo-data@users.noreply.github.com>
2023-02-24 21:08:12 -0300

    Update streamlit_app.py
    Create dataframes and show in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Create dataframes with pandas 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Show in a table
+streamlit.dataframe(fruityvice_normalized)
