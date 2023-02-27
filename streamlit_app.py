ffdc44f6050c544fe24847d412c74acb1952f901
marcelo-data <126196376+marcelo-data@users.noreply.github.com>
2023-02-27 12:22:37 -0300

    Update streamlit_app.py
    Modified add fruit err message

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   if not add_my_fruit:
-    streamlit.error("Please select a fruit to get information.")
+    streamlit.error("Enter a name of a fruit, please")
   else:
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
